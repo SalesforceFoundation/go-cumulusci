edc146092256a3f2e44dfaf3c62528d11d2930a9
ceiroa <crme1980@gmail.com>
2015-02-09 16:56:29 -0600

    moving handler into function

--- web.go
+++ web.go
@@ -7,16 +7,17 @@ import (
 	"os"
 )
 
-handler := &herokugoauth.Handler{
-	RequireDomain: "heroku.com",
-	Key:           os.Getenv("KEY"),
-	ClientID:      os.Getenv("OAUTH_CLIENT_ID"),
-	ClientSecret:  os.Getenv("OAUTH_CLIENT_SECRET"),
-}
-
 func main() {
 	http.HandleFunc("/", hello)
 	fmt.Println("listening...")
+
+	handler := &herokugoauth.Handler{
+		RequireDomain: "heroku.com",
+		Key:           os.Getenv("KEY"),
+		ClientID:      os.Getenv("OAUTH_CLIENT_ID"),
+		ClientSecret:  os.Getenv("OAUTH_CLIENT_SECRET"),
+	}
+
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), handler)
 	if err != nil {
 		panic(err)
@@ -29,5 +30,3 @@ func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "Authentication URL: "+herokugoauth.Endpoint.AuthURL)
 	fmt.Fprintln(res, "Token URL: "+herokugoauth.Endpoint.TokenURL)
 }
-
-
