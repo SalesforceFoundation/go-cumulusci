fb5b0443a3fda5c5bbab1189538be29ef64b1ebe
ceiroa <crme1980@gmail.com>
2015-02-10 23:20:08 -0600

    will it work on heroku?

--- web.go
+++ web.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -13,7 +15,7 @@ type Account struct {
 	Email string `json:"email"`
 }
 
-var store = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_SECRET")))
+var store = sessions.NewCookieStore([]byte(os.Getenv("KEY")))
 
 var conf = &oauth2.Config{
 	ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
@@ -47,29 +49,45 @@ func handleAuth(w http.ResponseWriter, r *http.Request) {
 	// Redirect user to consent page to ask for permission
 	// for the scopes specified above.
 	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	//fmt.Fprintln(res, "Visit the URL for the auth dialog: "+url)
 
 	body := `<a href="` + url + `">Sign in with Salesforce</a>`
 	w.Write([]byte(body))
-
-	// Use the authorization code that is pushed to the redirect URL.
-	// NewTransportWithCode will do the handshake to retrieve
-	// an access token and initiate a Transport that is
-	// authorized and authenticated by the retrieved token.
-	/**var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		panic(err)
-	}
-	tok, err := conf.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		panic(err)
-	}
-
-	client := conf.Client(oauth2.NoContext, tok)
-	client.Get("...")**/
 }
 
 func handleCallback(w http.ResponseWriter, r *http.Request) {
-	body := `<p>You have successfully authenticated!</p>`
-	w.Write([]byte(body))
+	// Get the authorization code.
+	code := r.FormValue("code")
+	/**access_token := r.FormValue("access_token")
+	refresh_token := r.FormValue("access_token")
+	body := `<p>Access Token: ` + access_token + `</p>`
+	body = body + `<p>Refresh Token: ` + refresh_token + `</p>`
+	body = body + `<p>Code: ` + code + `</p>`
+	w.Write([]byte(body))**/
+
+	//Exchange the received code for a token - this is the line that is failing
+	tok, err := conf.Exchange(oauth2.NoContext, code)
+	showError(w, err)
+
+	//Client returns an HTTP client using the provided token. The token will auto-refresh as necessary.
+	client := conf.Client(oauth2.NoContext, tok)
+
+	//we are hardcoding the server url!!! we need to change this!!!
+	url := fmt.Sprintf("https://na15.salesforce.com/services/data/32/query?q=%s", url.QueryEscape("select Id, name from account"))
+	resp, err := client.Get(url)
+	showError(w, err)
+
+	defer resp.Body.Close()
+	bodyArray, err := ioutil.ReadAll(resp.Body)
+	showError(w, err)
+
+	w.Write([]byte(bodyArray))
+}
+
+func showError(w http.ResponseWriter, err error) {
+	//var body string
+	if err != nil {
+		//body = `<p>There was an error: ` + err.Error() + `</p>`
+		//w.Write([]byte(body))
+		panic(err)
+	}
 }
